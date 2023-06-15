b6562756f1cce97784d901f279ca03821ef50489
maksmoroz91 <dlkontakta7@gmail.com>
2023-06-15 15:30:07 +0300

    add binance

---
+++ src/exchanges/binance/binance.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import * as ccxt from "ccxt";
+
+@Injectable()
+export class BinanceService {
+    private readonly exchange: ccxt.Exchange;
+
+    constructor() {
+        this.exchange = new ccxt.binance();
+    }
+
+    async getAllTokens(): Promise<{ symbol: string; price: number }[]> {
+        const tradingPairs = await this.exchange.fetchTickers(); // Получаем данные о торговых парах
+        const tokensWithUsdtPrice = Object.keys(tradingPairs)
+            .filter(symbol => symbol.endsWith('/USDT'))
+            .map(symbol => {
+                const ticker = tradingPairs[symbol];
+                const baseSymbol = symbol.replace('/USDT', ''); // Удаляем '/USDT' из названия пары
+                return {
+                    symbol: baseSymbol,
+                    price: ticker.last,
+                };
+            })
+            .filter(token => token.price !== undefined); // Фильтруем по наличию цены
+
+
+        return tokensWithUsdtPrice;
+    }
+}
